15add484624d5546eda1119026f78b167ef25a4b
Carlos Palmero Gonzalez <carlospalmerogonzalez@gmail.com>
2026-03-15 03:48:00 +0000

    [B] Modificacion nav_graph_mascota y MascotaContainerFragment

--- app/src/main/java/com/example/vetycare/ui/container/MascotaContainerFragment.kt
+++ app/src/main/java/com/example/vetycare/ui/container/MascotaContainerFragment.kt
@@ -1,14 +1,40 @@
 package com.example.vetycare.ui.container
 
 
+import android.media.Image
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
 import com.example.vetycare.R
 
 class MascotaContainerFragment : Fragment (R.layout.fragment_container_mascota) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val navHostFragment = childFragmentManager
+            .findFragmentById(R.id.container_host_mascota) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        val botonCita = view?.findViewById<ImageButton>(R.id.btnCita)
+        val botonTratamiento = view?.findViewById<ImageButton>(R.id.btnTratamiento)
+        val botonInforme = view?.findViewById<ImageButton>(R.id.btnInforme)
+        val botonMascota = view?.findViewById<ImageButton>(R.id.btnMascotas)
+
+        botonCita?.setOnClickListener {
+            navController.navigate(R.id.MascotaCitaFragment)
+        }
+        botonTratamiento?.setOnClickListener {
+            navController.navigate(R.id.MascotaTratamientoFragment)
+        }
+        botonInforme?.setOnClickListener {
+            navController.navigate(R.id.MascotaInformeFragment)
+        }
+        botonMascota?.setOnClickListener {
+            navController.navigate(R.id.MascotaPerfilFragment)
+        }
     }
 }
