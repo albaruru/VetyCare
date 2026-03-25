ba82d362386c9adf31497aef90342082591b52f5
albaruru <albaruru@gmail.com>
2026-03-25 13:46:38 +0100

    [V] Edicion del calendario

---
+++ app/src/main/java/com/example/vetycare/ui/container/CitasViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.vetycare.ui.container
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.vetycare.model.Cita
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class CitasViewModel : ViewModel() {
+
+    private val repository = CitasRepository()
+
+    private val _todasLasCitas = MutableStateFlow<List<Cita>>(emptyList())
+    val todasLasCitas: StateFlow<List<Cita>> = _todasLasCitas
+
+    private val _citasDelDia = MutableStateFlow<List<Cita>>(emptyList())
+    val citasDelDia: StateFlow<List<Cita>> = _citasDelDia
+
+    init {
+        cargarCitas()
+    }
+
+    private fun cargarCitas() {
+        viewModelScope.launch {
+            _todasLasCitas.value = repository.getCitasDelUsuario()
+        }
+    }
+
+    fun seleccionarDia(fecha: String) {
+        _citasDelDia.value = _todasLasCitas.value.filter {
+            it.fechaHoraInicio.startsWith(fecha)
+        }
+    }
+}
