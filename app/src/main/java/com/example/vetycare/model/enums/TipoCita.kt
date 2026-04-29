30d5394212831f0984632ec436f857a27bac14dc
albaruru <albaruru@gmail.com>
2026-04-29 20:48:37 +0200

    [D] revision de comits enums y relational

--- app/src/main/java/com/example/vetycare/model/enums/TipoCita.kt
+++ app/src/main/java/com/example/vetycare/model/enums/TipoCita.kt
@@ -1,6 +1,12 @@
 package com.example.vetycare.model.enums
 import com.example.vetycare.R
 
+/*
+    EXPLICACION
+        Enum que representa los tipos de cita disponibles en el sistema.
+        Cada tipo está asociado a un color para su representación en la interfaz.
+ */
+
 enum class TipoCita(val colorRes: Int) {
     VACUNACION(R.color.Vacunacion),
     REVISION(R.color.Revision),
--- app/src/main/java/com/example/vetycare/model/relational/CitaVeterinario.kt
+++ app/src/main/java/com/example/vetycare/model/relational/CitaVeterinario.kt
@@ -1,8 +1,12 @@
 package com.example.vetycare.model.relational
 
+/*
+    EXPLICACION
+        Modelo relacional que representa la información básica del veterinario asociado a una cita.
+        Incluye datos simplificados como nombre y apellido.
+ */
+
 class CitaVeterinario (
     var nombre: String? = null,
     var apellido: String? = null
 )
-{
-}
